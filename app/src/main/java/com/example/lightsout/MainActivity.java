bf8ee9da6c712c21b73f99574204be8d4aef5795
estebanparrarodriguez <esteban.parrarodriguez@belmont.edu>
2023-05-19 10:31:29 -0500

    App skeleton with GridLayout created and starter code for coloring buttons

---
+++ app/src/main/java/com/example/lightsout/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.lightsout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int GRID_SIZE = 3;
+    private GridLayout grid;
+    private boolean cellState [][];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        cellState = new boolean[][]{{true, true, true}, {true, true, true}, {true, true, true}};
+
+        setContentView(R.layout.activity_main);
+        grid = findViewById(R.id.light_grid);
+
+//      randomize();
+
+        recolor();
+    }
+
+    public void recolor(){
+        for (int i = 0; i < grid.getChildCount(); i++) {
+            Button gridButton = (Button) grid.getChildAt(i);
+
+            // Find the button's row and col
+            int row = i / GRID_SIZE;
+            int col = i % GRID_SIZE;
+
+            if (cellState[row][col] == true) {
+                gridButton.setBackgroundColor(getColor(R.color.blue_500));
+            } else {
+                gridButton.setBackgroundColor(getColor(R.color.black));
+            }
+        }
+    }
+
+    public void randomize(){
+        Random random = new Random();
+        for(int i =0; i< GRID_SIZE; i++){
+            for(int j =0; j< GRID_SIZE; j++){
+                cellState[i][j] = random.nextBoolean();
+            }
+        }
+    }
+
+
+
+}
